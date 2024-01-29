3395b4b0a3de2b92452258db9e3fbe96e5bdea3a
Liubov Revunova <lrevunova@mail.ru>
2024-01-29 15:36:35 +0300

    мой первый коммит

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const creationDateObj = {
+  year: 1899
+};
+
+const creationDate = new Date(
+  creationDateObj.year
+);
+
+const timeLeft = document.querySelector('#timeLeft');
+
+setInterval(() => {
+
+  const now = new Date();
+  const diffInMS = now - creationDate;
+  const diffAsDate = new Date(diffInMS);
+
+  const [
+    years
+  ] = diffAsDate
+      .toISOString()
+      .split('T')
+      .map(timePart =>
+        timePart
+          .split(/-|:|\./)
+          .map(str => +str)
+      )
+      .flat();
+
+  timeLeft.textContent = `${years - 1899} года `;
+
+}, 1000);
